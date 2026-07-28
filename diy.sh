97952d841f73b939f0c23e75487536c5ed021a70
wf-1223 <xuefeng_1223@163.com>
2026-07-28 16:24:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 set -e
 
-# Git网络优化，防止clone超时
+# Git网络优化
 git config --global http.version HTTP/1.1
 git config --global http.postBuffer 1048576000
 git config --global core.compression 0
 
 # 加载京东云一代 RE-SP-01B 适配补丁
 git clone https://github.com/yjzzjy4/JDCloud-RE-SP-01B.git jdcloud-patch
-cp -rf jdcloud-patch/target/linux/ramips/* target/linux/ramips/
-cp -rf jdcloud-patch/package/* package/
+echo "===== 查看补丁仓库目录 ====="
+ls -la jdcloud-patch/
+
+# 【重点】先看日志输出，确认里面文件夹名称！
+# 如果补丁仓库内直接是 target/ 就保留；
+# 如果里面是 openwrt/target/ 后续我再调整
+if [ -d "jdcloud-patch/target" ];then
+  cp -rf jdcloud-patch/target/* target/
+fi
+if [ -d "jdcloud-patch/package" ];then
+  cp -rf jdcloud-patch/package/* package/
+fi
+
 rm -rf jdcloud-patch
 
 # 拉取Argon主题
@@ -22,7 +33,7 @@ cp -rf files/* ./
 # 更新软件包feeds
 ./scripts/feeds update -a
 
-# ===== 移到feeds update之后！此时repositories.conf才存在 =====
+# 替换软件源
 sed -i 's#https://downloads.immortalwrt.org#https://mirrors.pku.edu.cn/immortalwrt#g' feeds/distfeeds.conf
 
 ./scripts/feeds install -a
